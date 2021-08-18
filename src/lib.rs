16312e4f56af136257d283c3327f84e33e2d83f9
Nick Cameron <nrc@ncameron.org>
2021-08-18 16:49:01 +1200

    Adapt latest dyno
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(core_intrinsics)]
 #![feature(const_type_id)]
 #![feature(const_type_name)]
+#![allow(incomplete_features)]
+#![feature(trait_upcasting)]
 
 /// https://doc.rust-lang.org/nightly/std/any/index.html
 pub mod any {
@@ -129,78 +131,134 @@ pub mod any {
 }
 
 /// https://github.com/mystor/dyno/tree/min_magic
-pub mod dyno {
+pub mod typed_provider {
     use crate::any::TypeId;
-    use core::marker::PhantomData;
 
-    /// This trait is implemented by specific `Tag` types in order to allow
+    /// This trait is implemented by specific `TypeTag` types in order to allow
     /// describing a type which can be requested for a given lifetime `'a`.
     ///
-    /// A few example implementations for type-driven `Tag`s can be found in the
+    /// A few example implementations for type-driven `TypeTag`s can be found in the
     /// [`tag`] module, although crates may also implement their own tags for more
     /// complex types with internal lifetimes.
-    pub trait Tag<'a>: Sized + 'static {
-        /// The type of values which may be tagged by this `Tag` for the given
+    pub trait TypeTag<'a>: Sized + 'static {
+        /// The type of values which may be tagged by this `TypeTag` for the given
         /// lifetime.
         type Type: 'a;
     }
 
-    /// Type-based `Tag` for `&'a T` types.
-    pub struct Ref<T: ?Sized + 'static>(PhantomData<T>);
+    pub mod tags {
+        use super::TypeTag;
+        use core::marker::PhantomData;
 
-    impl<'a, T: ?Sized + 'static> Tag<'a> for Ref<T> {
-        type Type = &'a T;
+        /// Type-based `TypeTag` for `&'a T` types.
+        pub struct Ref<T: ?Sized + 'static>(PhantomData<T>);
+
+        impl<'a, T: ?Sized + 'static> TypeTag<'a> for Ref<T> {
+            type Type = &'a T;
+        }
+
+        /// Type-based `TypeTag` for `&'a mut T` types.
+        pub struct RefMut<T: ?Sized + 'static>(PhantomData<T>);
+
+        impl<'a, T: ?Sized + 'static> TypeTag<'a> for RefMut<T> {
+            type Type = &'a mut T;
+        }
+
+        /// Type-based `TypeTag` for static `T` types.
+        pub struct Value<T: 'static>(PhantomData<T>);
+
+        impl<'a, T: 'static> TypeTag<'a> for Value<T> {
+            type Type = T;
+        }
+
+        /// Tag combinator to wrap the given tag's value in an `Option<T>`
+        pub struct Optional<I>(PhantomData<I>);
+
+        impl<'a, I: TypeTag<'a>> TypeTag<'a> for Optional<I> {
+            type Type = Option<I::Type>;
+        }
     }
 
-    /// Type-based `Tag` for `&'a mut T` types.
-    pub struct RefMut<T: ?Sized + 'static>(PhantomData<T>);
+    /// An untyped request for a tagged value of a specific type.
+    pub struct Requisition<'a, 'b>(&'b mut RequisitionImpl<dyn Tagged<'a> + 'a>);
+    // TODO is this useful? What about a Send or Sync version?
+    // pub struct SendRequisition<'a, 'b>(&'b mut RequisitionImpl<dyn Tagged<'a> + 'a + Sync>);
 
-    impl<'a, T: ?Sized + 'static> Tag<'a> for RefMut<T> {
-        type Type = &'a mut T;
+    impl<'a, 'b> Requisition<'a, 'b> {
+        /// Attempts to provide a value with the given `TypeTag` to the request.
+        pub fn provide<I>(&mut self, value: I::Type) -> &mut Self
+        where
+            I: TypeTag<'a>,
+        {
+            if let Some(res @ TagValue(None)) = self.0.tagged.downcast_mut::<tags::Optional<I>>() {
+                res.0 = Some(value);
+            }
+            self
+        }
+
+        /// Attempts to provide a value with the given `TypeTag` to the request.
+        pub fn provide_with<I, F>(&mut self, f: F) -> &mut Self
+        where
+            I: TypeTag<'a>,
+            F: FnOnce() -> I::Type,
+        {
+            if let Some(res @ TagValue(None)) = self.0.tagged.downcast_mut::<tags::Optional<I>>() {
+                res.0 = Some(f());
+            }
+            self
+        }
+
+        /// nrc extension.
+        pub fn provide_value<T, F>(&mut self, f: F) -> &mut Self
+        where
+            T: 'static,
+            F: FnOnce() -> T,
+        {
+            self.provide_with::<tags::Value<T>, F>(f)
+        }
+
+        /// nrc extension.
+        pub fn provide_ref<T: ?Sized + 'static>(&mut self, value: &'a T) -> &mut Self {
+            self.provide::<tags::Ref<T>>(value)
+        }
     }
 
-    /// Type-based `Tag` for static `T` types.
-    pub struct Value<T: 'static>(PhantomData<T>);
+    /// A concrete request for a tagged value. Can be coerced to `Requisition<'a>` to be
+    /// passed to provider methods.
+    type ConcreteRequisition<'a, I> = RequisitionImpl<TagValue<'a, tags::Optional<I>>>;
 
-    impl<'a, T: 'static> Tag<'a> for Value<T> {
-        type Type = T;
-    }
-
-    /// Tag combinator to wrap the given tag's value in an `Option<T>`
-    pub struct Optional<I>(PhantomData<I>);
-
-    impl<'a, I: Tag<'a>> Tag<'a> for Optional<I> {
-        type Type = Option<I::Type>;
-    }
-
-    mod private {
-        pub trait Sealed {}
+    /// Implementation detail shared between `Requisition<'a>` and `ConcreteRequisition<'a, I>`.
+    ///
+    /// Generally this value is used through the `Requisition<'a>` type alias as a `&mut
+    /// Requisition<'a>` out parameter, or constructed with the `ConcreteRequisition<'a, I>`
+    /// type alias.
+    #[repr(transparent)]
+    struct RequisitionImpl<T: ?Sized> {
+        tagged: T,
     }
 
     /// Sealed trait representing a type-erased tagged object.
     ///
     /// This trait is exclusively implemented by the `TagValue` type, and cannot be
     /// implemented outside of this crate due to being sealed.
-    pub unsafe trait Tagged<'a>: private::Sealed + 'a {
-        /// The `TypeId` of the `Tag` this value was tagged with.
+    unsafe trait Tagged<'a>: 'a {
+        /// The `TypeId` of the `TypeTag` this value was tagged with.
         fn tag_id(&self) -> TypeId;
     }
 
     /// A concrete tagged value for a given tag `I`.
     ///
     /// This is the only type which implements the `Tagged` trait, and encodes
-    /// additional information about the specific `Tag` into the type. This allows
+    /// additional information about the specific `TypeTag` into the type. This allows
     /// for multiple different tags to support overlapping value ranges, for
     /// example, both the `Ref<str>` and `Value<&'static str>` tags can be used to
     /// tag a value of type `&'static str`.
     #[repr(transparent)]
-    pub struct TagValue<'a, I: Tag<'a>>(pub I::Type);
-
-    impl<'a, I: Tag<'a>> private::Sealed for TagValue<'a, I> where I: Tag<'a> {}
+    struct TagValue<'a, I: TypeTag<'a>>(I::Type);
 
     unsafe impl<'a, I> Tagged<'a> for TagValue<'a, I>
     where
-        I: Tag<'a>,
+        I: TypeTag<'a>,
     {
         fn tag_id(&self) -> TypeId {
             TypeId::of::<I>()
@@ -212,9 +270,9 @@ pub mod dyno {
             impl<'a> $T {
                 /// Returns `true` if the dynamic type is tagged with `I`.
                 #[inline]
-                pub fn is<I>(&self) -> bool
+                fn is<I>(&self) -> bool
                 where
-                    I: Tag<'a>,
+                    I: TypeTag<'a>,
                 {
                     self.tag_id() == TypeId::of::<I>()
                 }
@@ -222,25 +280,9 @@ pub mod dyno {
                 /// Returns some reference to the dynamic value if it is tagged with `I`,
                 /// or `None` if it isn't.
                 #[inline]
-                pub fn downcast_ref<I>(&self) -> Option<&TagValue<'a, I>>
+                fn downcast_mut<I>(&mut self) -> Option<&mut TagValue<'a, I>>
                 where
-                    I: Tag<'a>,
-                {
-                    if self.is::<I>() {
-                        // SAFETY: Just checked whether we're pointing to a
-                        // `TagValue<'a, I>`.
-                        unsafe { Some(&*(self as *const Self as *const TagValue<'a, I>)) }
-                    } else {
-                        None
-                    }
-                }
-
-                /// Returns some reference to the dynamic value if it is tagged with `I`,
-                /// or `None` if it isn't.
-                #[inline]
-                pub fn downcast_mut<I>(&mut self) -> Option<&mut TagValue<'a, I>>
-                where
-                    I: Tag<'a>,
+                    I: TypeTag<'a>,
                 {
                     if self.is::<I>() {
                         // SAFETY: Just checked whether we're pointing to a
@@ -255,168 +297,65 @@ pub mod dyno {
     }
 
     tagged_methods!(
-        dyn Tagged<'a>,
-        dyn Tagged<'a> + Send,
-        dyn Tagged<'a> + Sync,
-        dyn Tagged<'a> + Send + Sync
+        dyn Tagged<'a> //,
+        // we only need these if we will pass Requisitions between threads,
+        // which we don't for Error, but might for async.
+                       // dyn Tagged<'a> + Send,
+                       // dyn Tagged<'a> + Sync,
+                       // dyn Tagged<'a> + Send + Sync
     );
-}
-
-pub mod provider {
-    use crate::dyno::{Optional, Ref, Tag, TagValue, Tagged, Value};
-
-    /// Implementation detail shared between `Request<'a>` and `ConcreteRequest<'a, I>`.
-    ///
-    /// Generally this value is used through the `Request<'a>` type alias as a `&mut
-    /// Request<'a>` out parameter, or constructed with the `ConcreteRequest<'a, I>`
-    /// type alias.
-    #[repr(transparent)]
-    pub struct RequestImpl<T: ?Sized> {
-        tagged: T,
-    }
-
-    /// An untyped request for a tagged value of a specific type.
-    pub type Request<'a> = RequestImpl<dyn Tagged<'a> + 'a>;
-
-    /// A concrete request for a tagged value. Can be coerced to `Request<'a>` to be
-    /// passed to provider methods.
-    pub type ConcreteRequest<'a, I> = RequestImpl<TagValue<'a, Optional<I>>>;
-
-    impl<'a> Request<'a> {
-        /// Check if the request is for a value with the given tag `I`. If it is,
-        /// returns `true`.
-        pub fn is<I>(&self) -> bool
-        where
-            I: Tag<'a>,
-        {
-            self.tagged.is::<Optional<I>>()
-        }
-
-        /// Attempts to provide a value with the given `Tag` to the request.
-        pub fn provide<I>(&mut self, value: I::Type) -> &mut Self
-        where
-            I: Tag<'a>,
-        {
-            if let Some(res @ TagValue(None)) = self.tagged.downcast_mut::<Optional<I>>() {
-                res.0 = Some(value);
-            }
-            self
-        }
-
-        /// Attempts to provide a value with the given `Tag` to the request.
-        pub fn provide_with<I, F>(&mut self, f: F) -> &mut Self
-        where
-            I: Tag<'a>,
-            F: FnOnce() -> I::Type,
-        {
-            if let Some(res @ TagValue(None)) = self.tagged.downcast_mut::<Optional<I>>() {
-                res.0 = Some(f());
-            }
-            self
-        }
-
-        /// nrc extension.
-        pub fn provide_value<T, F>(&mut self, f: F) -> &mut Self
-        where
-            T: 'static,
-            F: FnOnce() -> T,
-        {
-            if let Some(res @ TagValue(None)) = self.tagged.downcast_mut::<Optional<Value<T>>>() {
-                res.0 = Some(f());
-            }
-            self
-        }
-
-        /// nrc extension.
-        pub fn provide_ref<T: ?Sized + 'static>(&mut self, value: &'a T) -> &mut Self {
-            if let Some(res @ TagValue(None)) = self.tagged.downcast_mut::<Optional<Ref<T>>>() {
-                res.0 = Some(value);
-            }
-            self
-        }
-    }
-
-    impl<'a, I> ConcreteRequest<'a, I>
-    where
-        I: Tag<'a>,
-    {
-        /// Construct a new unfulfilled concrete request for the given type. This
-        /// can be coerced to a `Request<'a>` to pass to a type-erased provider
-        /// method.
-        pub fn new() -> Self {
-            RequestImpl {
-                tagged: TagValue(None),
-            }
-        }
-
-        /// Take any provided value from this concrete request.
-        pub fn take(self) -> Option<I::Type> {
-            self.tagged.0
-        }
-    }
 
     /// Trait implemented by a type which can dynamically provide tagged values.
     pub trait Provider {
-        fn provide<'a>(&'a self, request: &mut Request<'a>);
+        fn provide<'a>(&'a self, req: Requisition<'a, '_>);
     }
 
-    impl dyn Provider {
-        /// Request a specific value by a given tag from the `Provider`.
-        pub fn request<'a, I>(&'a self) -> Option<I::Type>
-        where
-            I: Tag<'a>,
-        {
-            let mut request = <ConcreteRequest<'a, I>>::new();
-            self.provide(&mut request);
-            request.take()
-        }
+    /// Request a specific value by a given tag from the `Provider`.
+    pub fn request_by_type_tag<'a, I>(provider: &'a dyn Provider) -> Option<I::Type>
+    where
+        I: TypeTag<'a>,
+    {
+        let mut req: ConcreteRequisition<'a, I> = RequisitionImpl {
+            tagged: TagValue(None),
+        };
+        provider.provide(Requisition(&mut req));
+        req.tagged.0
     }
 }
 
 pub mod error {
-    use crate::dyno::{Ref, Tag, Value};
-    use crate::provider::{ConcreteRequest, Provider, Request};
+    use crate::typed_provider::{request_by_type_tag, tags, Provider, Requisition, TypeTag};
     use core::fmt::Debug;
 
-    pub trait MyError: Debug + Provider {
-        fn provide_context<'a>(&'a self, request: &mut Request<'a>);
+    pub trait Error: Debug + Provider {
+        fn provide_context<'a>(&'a self, _req: Requisition<'a, '_>) {}
     }
 
-    impl<T: MyError> Provider for T {
-        fn provide<'a>(&'a self, request: &mut Request<'a>) {
-            self.provide_context(request);
+    impl<T: Error> Provider for T {
+        fn provide<'a>(&'a self, req: Requisition<'a, '_>) {
+            self.provide_context(req);
         }
     }
 
-    impl dyn MyError {
-        pub fn context_by_type_tag<'a, I>(&'a self) -> Option<I::Type>
-        where
-            I: Tag<'a>,
-        {
-            let mut request = <ConcreteRequest<'a, I>>::new();
-            self.provide(&mut request);
-            request.take()
+    impl dyn Error {
+        pub fn get_context_by_type_tag<'a, I: TypeTag<'a>>(&'a self) -> Option<I::Type> {
+            request_by_type_tag::<'_, I>(self)
         }
 
-        pub fn context_value<T: 'static>(&self) -> Option<T> {
-            let mut request = <ConcreteRequest<'_, Value<T>>>::new();
-            self.provide(&mut request);
-            request.take()
+        pub fn get_context<T: 'static>(&self) -> Option<T> {
+            request_by_type_tag::<'_, tags::Value<T>>(self)
         }
 
-        pub fn context_ref<'a, T: ?Sized + 'static>(&'a self) -> Option<&'a T> {
-            let mut request = <ConcreteRequest<'a, Ref<T>>>::new();
-            self.provide(&mut request);
-            request.take()
+        pub fn get_context_ref<T: ?Sized + 'static>(&self) -> Option<&T> {
+            request_by_type_tag::<'_, tags::Ref<T>>(self)
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::dyno;
     use crate::error::*;
-    use crate::provider::Request;
+    use crate::typed_provider::{tags, Requisition};
 
     #[derive(Debug)]
     pub struct ConcreteError {
@@ -424,28 +363,27 @@ mod tests {
         array: Vec<String>,
     }
 
-    impl MyError for ConcreteError {
-        fn provide_context<'a>(&'a self, result: &mut Request<'a>) {
-            result
-                .provide_value(|| "Hello!".to_owned())
+    impl Error for ConcreteError {
+        fn provide_context<'a>(&'a self, mut req: Requisition<'a, '_>) {
+            req.provide_value(|| "Hello!".to_owned())
                 .provide_ref(&*self.name);
         }
     }
 
     #[test]
     fn it_works() {
-        let e: &dyn MyError = &ConcreteError {
+        let e: &dyn Error = &ConcreteError {
             name: "Bob".to_owned(),
             array: vec![],
         };
-        let s: String = e.context_value().unwrap();
+        let s: String = e.get_context().unwrap();
         assert_eq!(&s, "Hello!");
-        assert!(e.context_by_type_tag::<dyno::Value<i32>>().is_none());
-        let s: &str = e.context_ref().unwrap();
+        assert!(e.get_context_by_type_tag::<tags::Value<i32>>().is_none());
+        let s: &str = e.get_context_ref().unwrap();
         assert_eq!(s, "Bob");
-        // let a: &[String] = e.context_ref().unwrap();
+        // let a: &[String] = e.get_context_ref().unwrap();
         // assert_eq!(a.len(), 0);
-        // let s = e.context::<dyno::Ref<str>>().unwrap();
+        // let s = e.get_context_by_type_tag::<tags::Ref<str>>().unwrap();
         // assert_eq!(s, "Boo!");
     }
 }
