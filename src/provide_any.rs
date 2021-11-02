4f8912959c88ab5bccde6b4253959f7c881f85f9
Nick Cameron <nrc@ncameron.org>
2021-11-02 09:00:51 +0000

    Move some code around
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/provide_any.rs
+++ src/provide_any.rs
@@ -83,7 +83,21 @@ mod auto_trait_assert {
 }
 
 impl<'a, 'b> Requisition<'a, 'b> {
-    /// Attempts to provide a value with the given `TypeTag` to the request.
+    /// Provide a value or other type with only static lifetimes.
+    pub fn provide_value<T, F>(&mut self, f: F) -> &mut Self
+    where
+        T: 'static,
+        F: FnOnce() -> T,
+    {
+        self.provide_with::<tags::Value<T>, F>(f)
+    }
+
+    /// Provide a reference, note that the referee type must be bounded by `'static`, but may be unsized.
+    pub fn provide_ref<T: ?Sized + 'static>(&mut self, value: &'a T) -> &mut Self {
+        self.provide::<tags::Ref<T>>(value)
+    }
+
+    /// Provide a value with the given `TypeTag`.
     pub fn provide<I>(&mut self, value: I::Type) -> &mut Self
     where
         I: TypeTag<'a>,
@@ -94,7 +108,7 @@ impl<'a, 'b> Requisition<'a, 'b> {
         self
     }
 
-    /// Attempts to provide a value with the given `TypeTag` to the request.
+    /// Provide a value with the given `TypeTag`, using a closure to prevent unnecessary work.
     pub fn provide_with<I, F>(&mut self, f: F) -> &mut Self
     where
         I: TypeTag<'a>,
@@ -104,20 +118,6 @@ impl<'a, 'b> Requisition<'a, 'b> {
             res.0 = Some(f());
         }
         self
-    }
-
-    /// nrc extension.
-    pub fn provide_value<T, F>(&mut self, f: F) -> &mut Self
-    where
-        T: 'static,
-        F: FnOnce() -> T,
-    {
-        self.provide_with::<tags::Value<T>, F>(f)
-    }
-
-    /// nrc extension.
-    pub fn provide_ref<T: ?Sized + 'static>(&mut self, value: &'a T) -> &mut Self {
-        self.provide::<tags::Ref<T>>(value)
     }
 }
 
